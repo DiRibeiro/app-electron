31b84185dc940da962af210f77f961708fd975ff
DiRibeiro <diribeiro740@gmail.com>
2020-03-12 16:26:41 -0300

    Open File in Tray

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-const { app, Menu, Tray} = require('electron')
+const { app, Menu, dialog, Tray} = require('electron')
 const path = require('path')
 
 let win
+
 app.on('ready', () => {
     const tray = new Tray(path.join(__dirname, 'icon.png'))
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item', type: 'normal' },
+        { label: 'Open File', click: onclick = () => {
+            dialog.showOpenDialogSync(contextMenu, {
+                properties: ['openFile'],
+                filters: ['*']
+            })           
+        } },
         { label: 'Item2', submenu: [
             { label: 'Item', type: 'normal' },
             { label: 'Item 2', type: 'checkbox' }
         ]}
       ])
-      // Call this again for Linux because we modified the context menu
-      tray.setContextMenu(contextMenu)
-      tray.setToolTip('Jogue Limpo com Osório')
+    
+    tray.setContextMenu(contextMenu)
+    tray.setToolTip('App Electron')
+    
 })
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
-    }
-})
-
-app.on('activate', () => {
-    if (win === null) {
-
     }
 })
